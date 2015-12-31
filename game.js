8cf8af4baa03a2fb260cdd3bff155560efe10d0f
Dorus <dpeelen@gmail.com>
2015-12-31 19:53:55 +0100

    fixed colors.

--- game.js
+++ game.js
@@ -15,17 +15,23 @@ $(window).load(function() {
       canvas.placeRect(e, 4);
     });
 
+  var colors = {
+    'red': "rgba(" + 255 + "," + 0 + "," + 0 + "," + 1 + ")",
+    'blue': "rgba(" + 0 + "," + 160 + "," + 200 + "," + 1 + ")",
+    'green': "rgba(" + 0 + "," + 255 + "," + 0 + "," + 1 + ")"
+  };
+
   $('input[name=color]', '#color').on('change', function() {
     var ctx = $("#theGame")[0].getContext("2d");
     switch ($('input[name=color]:checked', '#color').val()) {
       case "red":
-        ctx.fillStyle = "rgba(" + 255 + "," + 0 + "," + 0 + "," + 255 + ")";
+        ctx.fillStyle = colors.red;
         break;
       case "blue":
-        ctx.fillStyle = "rgba(" + 0 + "," + 160 + "," + 200 + "," + 255 + ")";
+        ctx.fillStyle = color.blue;
         break;
       case "green":
-        ctx.fillStyle = "rgba(" + 0 + "," + 255 + "," + 0 + "," + 255 + ")";
+        ctx.fillStyle = colors.green;
         break;
     }
   });
